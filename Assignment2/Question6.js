316c44455448596f813f6ad93e3763e8c2ee6925
Rohit0203 <56021080+Rohit0203@users.noreply.github.com>
2024-03-06 18:16:39 +0530

    Update Question6.js
    Added camel case

--- Assignment2/Question6.js
+++ Assignment2/Question6.js
@@ -1,6 +1,6 @@
 // Write a function expression that takes in a number and returns true if it's even and false if it's odd.
 
-const checkevenodd=function(value)
+const checkEvenOdd=function(value)
 {
     if(value%2)
     {
