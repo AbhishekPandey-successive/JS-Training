3f4f45a885f35a5ef0440ecafe18a17f14248c13
Abhishek Pandey <abhishek.pandey@successive.tech>
2024-03-06 15:03:02 +0530

    change file

--- Assignment2/Question5.js
+++
@@ -1,9 +0,0 @@
-// Function Expression which  takes two numbers and return their sum
-
-const myfunc=function(val1,val2)
-{
-    return val1+val2;
-}
-
-const answer=myfunc(2,3);
-console.log(answer);
