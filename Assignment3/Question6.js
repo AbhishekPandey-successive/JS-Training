4aa231c6d5390d98c4053c46beca8137056da7be
AbhishekPandey-successive <162098704+AbhishekPandey-successive@users.noreply.github.com>
2024-03-08 00:21:44 +0530

    Update Question6.js

--- Assignment3/Question6.js
+++ Assignment3/Question6.js
@@ -1,24 +1,59 @@
-function checkPrime(a)
-{
-    for(let i=2;i<=a-1;i++)
+// Approach 1:
+
+// function checkPrime(a)
+// {
+//     for(let i=2;i<=a-1;i++)
+//     {
+//         if(a%i==0)
+//         {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+
+// for(let i=2;i<=50;i++)
+// {
+//     if(checkPrime(i))
+//     {
+//         console.log(i)
+//     }
+// }
+
+
+// Approach 2:
+
+function sieveOfEratosthenes(n) {
+   
+    const primes = new Array(n + 1).fill(true);
+    primes[0] = primes[1] = false;
+
+    for (p = 2; p * p <= n; p++) 
     {
-        if(a%i==0)
+        
+        if (primes[p] == true) 
         {
-            return false;
+          
+            for (i = p * p; i <= n; i += p)
+                primes[i] = false;
         }
     }
 
-    return true;
-}
 
 
-for(let i=2;i<=50;i++)
-{
-    if(checkPrime(i))
+    for (i = 2; i <= n; i++)
     {
-        console.log(i)
+        if (primes[i] == true)
+            console.log(i );
     }
+
 }
+
+
+let n=50;
+sieveOfEratosthenes(n);
 
 
 
