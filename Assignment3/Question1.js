320ea6b32ff1fe22e64d2126dcaa8691400f597c
AbhishekPandey-successive <162098704+AbhishekPandey-successive@users.noreply.github.com>
2024-03-07 23:03:44 +0530

    Update Question1.js

--- Assignment3/Question1.js
+++ Assignment3/Question1.js
@@ -1,46 +1,13 @@
 //  program to perform functionality of a calculator (add,sub,multiply,divide)
 
-function addition(a,b)
-{
-    console.log(a+b)
+const calculatorObject={
+    '+':(a,b)=>a+b,
+    '-':(a,b)=>a-b,
+    '*':(a,b)=>a*b,
+    '/":(a,b)=>a/b
 }
 
-function subtraction(a,b)
-{
-    console.log(a-b)
-}
-
-function multiply(a,b)
-{
-    console.log(a*b)
-}
-
-function divide(a,b)
-{
-    console.log(a/b)
-}
-
-
-const operator=prompt("User enter the Opertaion to be per");
-
-switch(operator)
-{
-    case '+':
-        addition(2,4)
-        break;
-    
-    case '-' :
-        subtraction(2,4)
-        break;
-
-    case '*' :
-        multiply(2,4)
-        break;
-
-    case '/' :
-        divide()
-        break;
-
-    default:
-        console.log("Not Valid Operation")
-}
+console.log(calculatorObject['+'](2,3))
+console.log(calculatorObject['-'](2,3))
+console.log(calculatorObject['*'](2,3))
+console.log(calculatorObject['/'](2,3))
