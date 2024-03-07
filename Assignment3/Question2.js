18b17cfae840c4af494b719f552b1b087c57f269
AbhishekPandey-successive <162098704+AbhishekPandey-successive@users.noreply.github.com>
2024-03-08 00:46:14 +0530

    Update Question2.js

--- Assignment3/Question2.js
+++ Assignment3/Question2.js
@@ -6,30 +6,37 @@ let n = 4;
 let string = "";
 let count = 1;
 
-for (let i = 1; i <= n; i++) {
+for (let i = 1; i <= n; i++) 
+{
   
-  for (let j = 1; j <= i; j++) {
+  for (let j = 1; j <= i; j++) 
+  {
     string += count;
     count++;
   }
-  string += "\n";
+  console.log(string)
+  string="";
 }
-console.log(string);
+
+
 
 
 // Patter 2
 
 let n1 = 5; 
 let string1 = "";
-// External loop
-for (let i = 1; i <= n1; i++) {
-  // Internal loop
-  for (let j = 1; j <= i; j++) {
+
+for (let i = 1; i <= n1; i++) 
+{
+  let count=i
+  for (let j = 1; j <= i; j++) 
+  {
     string1 += i;
   }
-  string1 += "\n";
+  console.log(string1)
+  string1="";
 }
-console.log(string1);
+
 
 
 
