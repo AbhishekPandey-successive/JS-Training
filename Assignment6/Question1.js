281f34c2686b8641535ad7e4e872b5be91dddf6e
Abhishek Pandey <abhishek.pandey@successive.tech>
2024-03-14 14:22:41 +0530

    Question1 Updated

--- Assignment6/Question1.js
+++ Assignment6/Question1.js
@@ -1,30 +1,57 @@
 // 1. Create a class BankAccount with properties balance and owner and methods deposit(), withdraw(), and displayBalance().
 
-class BankAccount{
-    constructor(balance,owner)
-    {
-        this.balance=balance;
-        this.owner=owner;
+
+class BankAccount {
+
+    constructor(balance) {
+      this.balance = balance;
+      console.log(`Opening Balance: $${balance}`);
     }
-
-    deposit()
-    {
-        console.log(`Total deposit amount is balance 1000`)
+  
+    deposit(amount) {
+      this.balance += amount;
+      console.log(`Deposited: $${amount}`);
     }
-
-    withdraw()
-    {
-        console.log("Withdraw amount is 500");
+  
+    withdraw(amount) {
+      if (amount <= this.balance) {
+        this.balance -= amount;
+        console.log(`Withdrawn: $${amount}`);
+      } else {
+        console.log(`Want to withdraw: $${amount}`);
+        console.log('Insufficient balance');
+      }
     }
-
-    displayBalance()
-    {
-        console.log(`Balance amount is ${this.balance+1000-500}`)
+  
+    displayBalance() {
+      console.log(`Account Balance: $${this.balance}`);
     }
-}
+  }
+  
+  
+  const account = new BankAccount( 1500);
+  
+  
+  account.deposit(500);
+  
+  
+  account.withdraw(400);
+  
+  
+  account.displayBalance();
+  
+  
+  account.withdraw(1800);
+  
+  account.displayBalance();
 
-let userAccount=new BankAccount(10000,"Abhishek Pandey")
+  
 
-userAccount.deposit();
-userAccount.withdraw();
-userAccount.displayBalance();
+
+
+
+
+
+
+
+
