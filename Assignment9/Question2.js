2804b31d5cdd9e7cb08ae221bd49ffce2769f946
Abhishek Pandey <abhishek.pandey@successive.tech>
2024-03-27 14:41:06 +0530

    Assignment 9

---
+++ Assignment9/Question2.js
@@ -0,0 +1,11 @@
+
+// Question 2:
+
+const adition = require("lodash");
+ 
+let gfg = adition.sum([15, 10, 10, 20, 25]);
+ 
+
+console.log(gfg);
+
+
---
+++ Assignment9/math/math.mjs
@@ -0,0 +1,13 @@
+function addition(value1,value2)
+{
+    return value1+value2
+}
+
+
+function subtraction(value3,value4)
+{
+    return value3-value4
+}
+
+
+export  {addition,subtraction}
